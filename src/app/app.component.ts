6f856e7325b4d326d7c1b5ecb0264251f14a0864
Sahil pillania <sahilpillania88@gmail.com>
2023-06-15 14:11:19 +0530

    weather app UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { NgModule } from '@angular/core';
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faDroplet } from '@fortawesome/free-solid-svg-icons';
+import { faTemperatureQuarter } from '@fortawesome/free-solid-svg-icons';
+import { faTemperatureFull } from '@fortawesome/free-solid-svg-icons';
+import { faWind} from '@fortawesome/free-solid-svg-icons';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   title = 'weather_app_Angular';
+  icon = faCoffee;
+  faDroplet = faDroplet;
+  faTemperatureQuarter = faTemperatureQuarter;
+  faTemperatureFull= faTemperatureFull;
+  faWind = faWind;
 }
