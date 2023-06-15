39e102edee2acdb8c4ab41b489d12b53658f8025
Sahil pillania <sahilpillania88@gmail.com>
2023-06-15 23:14:04 +0530

    Added api data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ServiceService } from './services/service.service';
 import { NgModule } from '@angular/core';
 // import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
@@ -15,7 +15,36 @@ import { faWind} from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./app.component.css'],
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private ServiceService: ServiceService){
+  
+  }
+
+weatherData?: any;
+  ngOnInit(): void {
+   this.getWeatherData(this.cityName);
+   this.cityName = "";
+  
+  }
+
+  cityName: string = "Wellington";
+  private getWeatherData(cityName: string){
+   this.ServiceService.getWeatherData(cityName).subscribe({next:(res)=>{ 
+      this.weatherData = res;
+      console.log(res);
+      
+      
+    }
+    })
+  
+  }
+  onSubmit(){
+  this.getWeatherData(this.cityName);
+  this.cityName = "";
+  }
+
+
   title = 'weather_app_Angular';
   icon = faCoffee;
   faDroplet = faDroplet;
